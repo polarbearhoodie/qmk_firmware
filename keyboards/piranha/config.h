f3864eaf34e368bedc03f9786a205d1e1ca53667
Tavi <dev@silvermice.com>
2025-11-07 23:22:22 -0800

    Enabled split watchdog Hopefully this removes the constant need for unplugging and replugging

--- keyboards/piranha/config.h
+++ keyboards/piranha/config.h
@@ -6,3 +6,5 @@
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN GP26     // USART TX pin
 #define SERIAL_USART_RX_PIN GP27     // USART RX pin
+
+#define SPLIT_WATCHDOG_ENABLE
