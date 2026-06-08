320f60d3262404f5f1fa190c19b34be1965f4ac9
Tavi <dev@silvermice.com>
2026-06-07 22:32:47 -0700

    hammerhead keyboard, using rp2040

---
+++ keyboards/aluminum_owls/hammerhead/config.h
@@ -0,0 +1,10 @@
+//split handedness
+#define SPLIT_HAND_PIN GP29
+#define	SPLIT_HAND_PIN_LOW_IS_LEFT
+
+//configuring the serial driver
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN GP26     // USART TX pin
+#define SERIAL_USART_RX_PIN GP27     // USART RX pin
+
+#define SPLIT_WATCHDOG_ENABLE
